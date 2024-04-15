4e5d90a4fb014bcb24d6746853de8cd43c601a75
cvanballegooijen <cojan@hashicorp.com>
2024-04-15 13:34:33 +0200

    Add aws provider

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-west-1"
+}
+
+variable "vpc_external_cidr" {
+  type    = string
+  default = "10.64.0.0/16"
+}
+
+variable "vpc_internal_cidr" {
+  type    = string
+  default = "10.65.0.0/16"
+}
+
+variable "tgw_amazon_side_asn" {
+  type    = string
+  default = "65101"
+}
