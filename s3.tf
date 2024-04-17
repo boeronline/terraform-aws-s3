8794630cf2d58c4d5a711cd7c0307a0156a027ba
Steven <96180461+steven-deboer@users.noreply.github.com>
2024-04-17 10:02:49 +0200

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "prismacloud-runtask-s3" {
    bucket = "prismacloud-runtask-s3-shorttrack-session"
 
    tags = {
-      Environment = "dev"
+      Environment = "prod"
       Owner = "apps-team"
     }
 }
